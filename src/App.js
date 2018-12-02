b826d80f6b5f553dcba10d63bab10265e2dc267e
sarathsprakash <sarathsprakash@gmail.com>
2018-12-02 13:28:22 +0530

    changed app text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hi there,I am sarath
+            Hi there,I am sarath. Welcome!!
           </p>
           <a
             className="App-link"
